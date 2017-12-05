b4f00573609ed3f7ecb2d63983b2d0b33fe614a6
notjamesheck <224@holberton.com>
2017-12-05 05:49:16 +0000

    8

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
+str = "Python is an interpreted, interactive, object-oriented programming\
+language that combines remarkable power with very clear syntax"
 listed = str.split(' ')
 str = ("{} {} {} {}".format(listed[5], listed[6], listed[-4], listed[0]))
 print(str)
