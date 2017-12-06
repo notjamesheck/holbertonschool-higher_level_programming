d0960983a4036fe42b2b3aa623ff53b4502f07c1
notjamesheck <224@holberton.com>
2017-12-06 15:06:36 +0000

    8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+def uppercase(str):
+    s = list(str)
+    for l in range(len(s)):
+        if ((ord(s[l]) >= 97) and (ord(s[l]) <= 122)):
+                s[l] = ord(s[l]) - 32
+                s[l] = chr(s[l])
+    print(''.join(s))
+    
