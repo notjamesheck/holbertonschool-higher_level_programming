b511dc2beefd86a78dbcbd540c089ac518b3d909
notjamesheck <224@holberton.com>
2017-12-06 15:23:51 +0000

    9

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    num = number
+    if num < 0:
+        num = abs(num) % 10
+    else:
+        num = num % 10
+    print(num, end='')
+    return num
+
