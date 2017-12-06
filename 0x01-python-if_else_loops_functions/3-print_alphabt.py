c97f4999a9553b44c2aa0484732b0d59e058a3d6
notjamesheck <224@holberton.com>
2017-12-06 05:59:23 +0000

    1-5

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+for i in range(97, 122):
+	if ((chr(i) != 'q') and (chr(i) != 'e')): 
+		print(chr(i), end='')
+
---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,2 @@
+#!/usr/bin/python3
+print(*list(range(00, 100)), sep=', ')
