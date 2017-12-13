219564d6fb79da0c5ec63629ac6c9b97e7975400
notjamesheck <224@holbertonschool.com>
2017-12-13 19:19:38 +0000

    3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-import sys
-sum = 0
-if len(sys.argv) > 1:
-    for x in sys.argv:
-        mint = eval(x)
-        sum += mint
+if __name__ == "__main__":
+    import sys
+    sum = 0
+    if len(sys.argv) > 1:
+        for x in sys.argv[1:]:
+            sum += int(x)
+        print("{}".format(sum))
+
+    elif len(sys.argv) <= 1:
+        print("0")
