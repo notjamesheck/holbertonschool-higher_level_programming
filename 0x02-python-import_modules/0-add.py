b60a67d6e5b5991f92557204a97d5a05e4cdd1bb
notjamesheck <224@holbertonschool.com>
2017-12-13 13:47:11 +0000

    0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,6 @@
-#!/bin/usr/python3
+#!/usr/bin/python3
 from add_0 import add
 
-
-def add(a, b):
-    a = 1
-    b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+a = 1
+b = 2
+print("{} + {} = {}".format(a, b, a+b))
