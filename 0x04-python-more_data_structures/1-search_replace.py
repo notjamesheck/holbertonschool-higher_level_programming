48fe65779b5b9d4931386eeaf4da4d2a1c8f713b
notjamesheck <224@holbertonschool.com>
2017-12-14 23:55:17 +0000

    1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = my_list[:]
-    new_list[search] = replace
-    return new_list
+    newl = my_list[:]
+    for i in newl:
+        if newl[i] == newl[search-1]:
+            newl[search-1] = replace
+            break
+    return newl
