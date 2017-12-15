df3b16f4b0909e814c082377ee5340c20afd68b8
notjamesheck <224@holbertonschool.com>
2017-12-15 01:26:18 +0000

    3

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
+    if not set_1:
+        return
+    if not set_2:
+        return
     for i in set_1:
         for j in set_2:
             if i == j:
-                return i
+                return i 
