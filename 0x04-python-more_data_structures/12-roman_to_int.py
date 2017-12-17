822d5bba06aa8a1f1207c79fa01dd4dc1b5de878
notjamesheck <224@holbertonschool.com>
2017-12-17 23:19:31 +0000

    12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -15,7 +15,25 @@ def roman_to_int(roman_string):
                 summ += currennt
         summ += rom[rom_string[-1]]
         return summ
+    else:
+        return 0
 
 if __name__ == "__main__":
+    rom_num = "I"
+    print("{} = {}".format(rom_num, roman_to_int(rom_num)))
+    rom_num = "III"
+    print("{} = {}".format(rom_num, roman_to_int(rom_num)))
+    rom_num = "XXI"
+    print("{} = {}".format(rom_num, roman_to_int(rom_num)))
+    rom_num = "IV"
+    print("{} = {}".format(rom_num, roman_to_int(rom_num)))
+    rom_num = "CXXIV"
+    print("{} = {}".format(rom_num, roman_to_int(rom_num)))
+    rom_num = 657624
+    print("{} = {}".format(rom_num, roman_to_int(rom_num)))
+    rom_num = None
+    print("{} = {}".format(rom_num, roman_to_int(rom_num)))
     rom_num = "XCIX"
+    print("{} = {}".format(rom_num, roman_to_int(rom_num)))
+    rom_num = "LXXXIX"
     print("{} = {}".format(rom_num, roman_to_int(rom_num)))
