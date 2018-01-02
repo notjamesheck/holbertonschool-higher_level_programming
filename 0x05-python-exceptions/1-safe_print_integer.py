7a6134574a581c88fe62b28ca336ce3e086d2c2f
notjamesheck <224@holberton.com>
2018-01-02 21:51:17 +0000

    1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,9 +2,9 @@
 
 
 def safe_print_integer(value):
-    while True:
         try:
             print("{:d}".format(value))
-            return True
-        except ValueError:
+        except:
             return False
+        else:
+            return True
