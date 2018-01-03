a7c3045d052de6212a5f8768013d6e6e79ce7cb6
notjamesheck <224@holberton.com>
2018-01-03 00:42:18 +0000

    2

--- 0x05-python-exceptions/2-main.py
+++ 0x05-python-exceptions/2-main.py
@@ -5,7 +5,6 @@ safe_print_list_integers = \
 my_list = [1, 2, 3, 4, 5]
 
 nb_print = safe_print_list_integers(my_list, 2)
-print(nb_print)
 print("nb_print: {:d}".format(nb_print))
 
 my_list = [1, 2, 3, "Holberton", 4, 5, [1, 2, 3]]
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     i = 0
+    index = 0 
     while True:
         try:
-            if x and my_list[i]):
-                if int(my_list[i]):
-                     print("{:d}".format(my_list[i]), end="")
+            if x and int(my_list[index]):
+                print("{:d}".format(my_list[i]), end="")
                 i += 1
                 x -= 1
+                index += 1
+        except (ValueError, TypeError):
+            continue
         except:
+            print()
             return i
