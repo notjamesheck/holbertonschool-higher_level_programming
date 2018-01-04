8e4765cb094703848c91f8526db0cf86fb0f27f7
notjamesheck <224@holberton.com>
2018-01-04 07:06:38 +0000

    6

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+class square that defines a square
+example of docstring on the __init__ method
+Args:
+    size (int): private instance attribute
+"""
+
+
+class Square():
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if type(size) != int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
+    def area(self):
+        return self.__size**2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for i in range(self.__size):
+                    print("#", end="")
+                print()
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        for i in value:
+            if i > 0:
+                raise TypeError("position must be a tuple of 2 positive integers")
+           
+        
