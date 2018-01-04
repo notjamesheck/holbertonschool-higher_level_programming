69672eb0e831db0139e0401f8c39525292f97f0e
notjamesheck <224@holberton.com>
2018-01-04 05:02:16 +0000

    1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+class square that defines a square
+example of docstring on the __init__ method
+Args:
+    size (int): no type/value verification
+"""
+
+
+class Square():
+    def __init__(self, size):
+        self.__size = size
