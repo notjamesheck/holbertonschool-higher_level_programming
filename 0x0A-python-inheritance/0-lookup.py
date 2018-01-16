c1eb26fa40a6feb6c6aa8708f8b8cf018f2017d5
notjamesheck <224@holberton.com>
2018-01-16 21:53:53 +0000

    repo

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""return list of available attributes and methods
+"""
+
+
+def lookup(obj):
+    """return list of available attributes and methods
+    """
+    return dir(obj)
