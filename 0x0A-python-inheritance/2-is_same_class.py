783eca875778dc2629790a414f00c57f7f3be297
notjamesheck <224@holberton.com>
2018-01-16 23:39:37 +0000

    1-4

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""something
+"""
+
+
+def is_same_class(obj, a_class):
+    """something
+    """
+    if type(obj) is a_class:
+        return True
+    else:
+        return False
---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""meh
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """meh
+    """
+    return isinstance(obj, a_class)
