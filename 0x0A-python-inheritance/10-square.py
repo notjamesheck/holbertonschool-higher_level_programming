dd6619d9c5bf2c43ea5dfa6dfb8dab354706db3b
notjamesheck <224@holberton.com>
2018-01-17 07:57:57 +0000

    folders and files

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,30 @@
+#!/ sr/bin/python3
+
+
+class BaseGeometry():
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+
+    def __init__(self, width=0, height=0):
+
+        self.integer_validator("width", width)
+        self.__width = width
+
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
