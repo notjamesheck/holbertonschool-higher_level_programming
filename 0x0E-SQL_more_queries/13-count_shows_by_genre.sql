ae8fef922fd525365d8a96637aead064b68ae8b8
notjamesheck <224@holbertonschool.com>
2018-03-02 01:00:10 +0000

    gah!

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,2 +1,2 @@
 -- comment
-SELECT name, COUNT(*) AS number_shows FROM tv_genres JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id GROUP BY genre_id ORDER BY COUNT(*) DESC;
+SELECT name AS genre, COUNT(*) AS number_shows FROM tv_genres JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id GROUP BY genre_id ORDER BY COUNT(*) DESC;
