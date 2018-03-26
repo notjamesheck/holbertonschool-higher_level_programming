098e27ba6b86c62921e40b9c51edb11aadb3f073
notjamesheck <224@holbertonschool.com>
2018-03-26 22:06:38 +0000

    1

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    Session = sessionmaker()
+    session = Session(bind=engine)
+
+    session.add(State(name='Louisiana'))
+    session.commit()
+
+    for state in session.query(State).order_by(State.name):
+        if state.name == 'Louisiana':
+            print(state.id)
+
+    session.close()
