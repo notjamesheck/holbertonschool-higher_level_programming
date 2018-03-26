dc0f26031ddc0a621315d0e29ed4d539b6752590
notjamesheck <224@holbertonschool.com>
2018-03-26 21:37:35 +0000

    10

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    Session = sessionmaker()
+    session = Session(bind=engine)
+
+    i = 0
+    for state in session.query(State).order_by(State.id):
+        if state.name == sys.argv[4]:
+            i = 1
+            print(state.id)
+
+    if i is 0:
+        print('Not found')
+
+    session.close()
