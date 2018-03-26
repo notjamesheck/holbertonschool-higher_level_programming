ce195d80a60f87712677e75f03b6b5f800b531b3
notjamesheck <224@holbertonschool.com>
2018-03-26 23:12:03 +0000

    43

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -16,6 +16,7 @@ if __name__ == '__main__':
     for poop in session.query(State).order_by(State.name):
         if 'a' in poop.name:
             session.delete(poop)
-            session.commit()
+
+    session.commit()
 
     session.close()
