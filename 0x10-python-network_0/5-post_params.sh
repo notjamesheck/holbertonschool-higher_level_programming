93834eb134d1450070eb156e21edf89ef791c867
notjamesheck <224@holbertonschool.com>
2018-04-04 06:38:24 +0000

    5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # cURL POST parameters
-curl -sX POST "$1" -H "email:hr@holbertonschool.com" "subject:I will always be here for PLD"
+curl -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" -Xs POST "$1"
