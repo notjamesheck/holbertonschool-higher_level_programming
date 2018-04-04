ab0b7db39dddd02caed279d0b8685f1c74a34c64
notjamesheck <224@holbertonschool.com>
2018-04-04 06:46:36 +0000

    5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # cURL POST parameters
-curl -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" -Xs POST "$1"
+curl -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" -H "Content-Type: application/x-www-form-urlencoded" -X POST "$1"
