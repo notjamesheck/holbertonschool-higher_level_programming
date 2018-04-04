2cb1d09a3395e297ac3efd1cd910300304c9af37
notjamesheck <224@holbertonschool.com>
2018-04-04 07:06:24 +0000

    65

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,3 +3,19 @@
     Find a peak
 '''
 
+def find_peak(list_of_integers):
+    '''
+    '''
+    if len(list_of_integers) is None:
+        '''
+        '''
+        return None
+
+    if len(list_of_integers) is 1:
+        '''
+        '''
+        return list_of_integers[0]
+
+    for num in list_of_integers:
+        '''
+        '''
