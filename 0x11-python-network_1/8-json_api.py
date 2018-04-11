b8a5e8c6a97e1314db7091295150f42b1e12ff2e
notjamesheck <224@holbertonschool.com>
2018-04-11 05:53:06 +0000

    8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,9 +7,9 @@ import sys
 
 if '__name__' == '__main__':
 
-    if sys.argv[1]:
+    if len(sys.argv) > 1:
         try:
-            r = requests.post('http://34.206.234.184:38297/search_user',
+            r = requests.post('http://0.0.0.0:5000/search_user',
                               data={'q': sys.argv[1]})
             print('[{}] {}'.format(r.json()['id'], r.json()['name']))
         except KeyError:
