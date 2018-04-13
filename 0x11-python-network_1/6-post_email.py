e528fe9c0c8c947a8eff9341f9e016eb7284d80e
notjamesheck <224@holbertonschool.com>
2018-04-13 20:13:44 +0000

    8

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -8,5 +8,5 @@ import sys
 if __name__ == "__main__":
 
     if len(sys.argv[1]) > 1:
-        r = requests.post(sys.argv[1], data={'email': sys.argv[1]})
+        r = requests.post(sys.argv[1], data={'email': sys.argv[2]})
         print(r.text)
