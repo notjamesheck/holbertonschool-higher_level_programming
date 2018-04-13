2e140a5be4052c82fc96e4580f8878829e1d6af8
notjamesheck <224@holbertonschool.com>
2018-04-13 19:42:29 +0000

    4

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -8,8 +8,8 @@ import sys
 
 if __name__ == "__main__":
 
-try:
-    with urllib.request.urlopen(sys.argv[1]) as status:
-            print(status.read().decode('utf-8'))
-except urllib.error.HTTPError as err:
-    print('Error code: {}'.format(err.code))
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as status:
+                print(status.read().decode('utf-8'))
+    except urllib.error.HTTPError as err:
+        print('Error code: {}'.format(err.code))
