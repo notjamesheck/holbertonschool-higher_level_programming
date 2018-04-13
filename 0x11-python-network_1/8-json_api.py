21eb281fb6075c6726cfd2c79464c6b574fea46f
notjamesheck <224@holbertonschool.com>
2018-04-13 17:10:36 +0000

    8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,14 +5,14 @@
 import requests
 import sys
 
-if '__name__' == '__main__':
+if __name__ == '__main__':
 
     if len(sys.argv) > 1:
         try:
-            r = requests.post('http://0.0.0.0:5000/search_user',
+            r = requests.post('http://172.31.54.208:38344/search_user',
                               data={'q': sys.argv[1]})
             print('[{}] {}'.format(r.json()['id'], r.json()['name']))
-        except KeyError:
+        except Exception:
             print('Not a valid JSON')
     else:
         print('No result')
