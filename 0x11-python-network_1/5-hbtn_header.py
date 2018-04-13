ceff2ce77e2ffaf6d438608e5072110701dd3522
notjamesheck <224@holbertonschool.com>
2018-04-13 19:52:50 +0000

    6

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -7,6 +7,6 @@ import sys
 
 if __name__ == "__main__":
 
-    r = requests.get(sys.argv[1])
-    if r.headers['X-Request-Id']:
-        print(r.headers['X-Request-Id'])
+    if sys.argv[1]:
+        r = requests.get(sys.argv[1])
+        print(r.headers.get('X-Request-Id'))
