70ee28f5d0227bc6469c286c32a4350b44bfcbb9
notjamesheck <224@holbertonschool.com>
2018-04-13 22:22:50 +0000

    042

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,21 +7,17 @@ import sys
 
 if __name__ == '__main__':
 
-    data = {'q': ""}
+    datal = {'q': ''}
     if len(sys.argv) > 1:
+        datal = {'q': sys.argv[1]}
         r = requests.post('http://172.31.54.208:38344/search_user',
-                          data={'q': sys.argv[1]})
-        '''
-        try:
-        '''
-        sumVar = r.json()
-        if sumVar:
+                          data=datal)
+
+    try:
+        if r.json():
+            sumVar = r.json()
             print('[{}] {}'.format(sumVar['id'], sumVar['name']))
         else:
-            print('Not a valid JSON')
-        '''
-        except Exception:
-            print('Not a valid JSON')
-        '''
-    else:
-        print('No result')
+            print('No result')
+    except:
+        print('Not a valid JSON')
--- 0x11-python-network_1/9-starwars.py
+++ 0x11-python-network_1/9-starwars.py
@@ -2,4 +2,9 @@
 '''
     comment
 '''
+import requests
+import sys
 
+if __name__ == '__main__':
+
+    
