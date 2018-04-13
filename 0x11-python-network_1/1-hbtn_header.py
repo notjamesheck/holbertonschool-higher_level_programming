bfa687d5d597a48b88b0a77a5ec778b26663d225
notjamesheck <224@holbertonschool.com>
2018-04-13 19:21:09 +0000

    1redo

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -8,5 +8,5 @@ import sys
 
 if __name__ == "__main__":
 
-with urllib.request.urlopen(sys.argv[1]) as resp:
-    print(resp.getheader('X-Request-Id'))
+    with urllib.request.urlopen(sys.argv[1]) as resp:
+        print(resp.getheader('X-Request-Id'))
