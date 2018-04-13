6270a3d865c81f4ea61814cb9098180a7d0c7ea6
notjamesheck <224@holbertonschool.com>
2018-04-13 22:29:45 +0000

    093

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     if len(sys.argv) > 1:
         datal = {'q': sys.argv[1]}
 
-    r = requests.post('http://172.31.54.208:38344/search_user',
+    r = requests.post('http://0.0.0.0:5000/search_user',
                       data=datal)
 
     try:
