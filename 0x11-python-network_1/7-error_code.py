9501344739c244a6b8314f09d2015905f026c184
notjamesheck <224@holbertonschool.com>
2018-04-13 20:17:06 +0000

    4

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,8 +7,8 @@ import sys
 
 if __name__ == "__main__":
 
-r = requests.get(sys.argv[1])
-if r.status_code is 200:
-    print(r.text)
-else:
-    print(r.status_code)
+    r = requests.get(sys.argv[1])
+    if r.status_code is 200:
+        print(r.text)
+    else:
+        print(r.status_code)
