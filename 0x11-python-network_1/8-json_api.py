6ef48aa273d43304121570078c963fe8172d0463
notjamesheck <224@holbertonschool.com>
2018-04-13 17:23:40 +0000

    8 again

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -8,11 +8,17 @@ import sys
 if __name__ == '__main__':
 
     if len(sys.argv) > 1:
+        r = requests.post('http://172.31.54.208:38344/search_user',
+                          data={'q': sys.argv[1]})
         try:
-            r = requests.post('http://172.31.54.208:38344/search_user',
-                              data={'q': sys.argv[1]})
-            print('[{}] {}'.format(r.json()['id'], r.json()['name']))
+            sumVar = r.json()
+            if sumVar:
+                print('[{}] {}'.format(sumVar['id'], sumVar['name']))
+            else:
+                print('No result')
+
         except Exception:
             print('Not a valid JSON')
     else:
+
         print('No result')
