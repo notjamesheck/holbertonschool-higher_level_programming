77136831eec74722aa1b080dfcb7797000b22bbf
notjamesheck <224@holbertonschool.com>
2018-04-14 02:20:19 +0000

    sW

--- 0x11-python-network_1/9-starwars.py
+++ 0x11-python-network_1/9-starwars.py
@@ -7,4 +7,10 @@ import sys
 
 if __name__ == '__main__':
 
-    
+    r = requests.get('https://swapi.co/api/people/?search={}'.
+                     format(sys.argv[1]))
+    print(r.status_code)
+    print(r.json()['count'])
+    results = r.json()['results']
+    for val in results:
+        print(val['name'])
