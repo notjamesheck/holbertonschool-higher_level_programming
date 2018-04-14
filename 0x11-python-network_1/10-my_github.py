235a92ec88072c21c53774775e16a9518166d8d5
notjamesheck <224@holbertonschool.com>
2018-04-14 03:09:19 +0000

    10

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+'''
+    comment
+'''
+import requests
+import sys
+
+if __name__ == '__main__':
+
+    r = requests.get('https://api.github.com/user',
+                     auth=(sys.argv[1], sys.argv[2]))
+    if r.json().get('id'):
+        print(r.json().get('id'))
+    else:
+        print('None')
