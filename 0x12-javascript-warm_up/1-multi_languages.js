9f30b95ece73c7b735550e29a5022f66cd8b526c
notjamesheck <224@holbertonschool.com>
2018-05-01 15:14:14 -0700

    exercise 1

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// comment
+
+let myVar = [];
+myVar.push('C is fun');
+myVar.push('Python is cool');
+myVar.push('Javascript is amazing');
+for (let i = 0; i <= 2; i++) {
+  console.log(myVar[i]);
+}
