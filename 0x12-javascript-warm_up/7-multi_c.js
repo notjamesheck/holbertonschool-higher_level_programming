2c999d8b495b5459c217b47c6bdcbb0d9ac5ea75
notjamesheck <224@holbertonschool.com>
2018-05-01 17:05:29 -0700

    exercise 7

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// comment
+
+let x = parseInt(process.argv[2]);
+if (!x) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
+}
