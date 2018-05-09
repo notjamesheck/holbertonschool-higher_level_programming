a8a7e450a7c3f810e88794b1adf8deee71cd32f0
notjamesheck <224@holbertonschool.com>
2018-05-08 22:43:35 -0700

    initial

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+// prints the number of argument already printed
+
+exports.logMe = function (item) {
+  let i = 0;
+  function poop (item) {
+    function poop (i) {
+      console.log(`${i++}: ${item}`);
+      return i++;
+    }
+    console.log(i);
+    return poop(i);
+  }
+  console.log(i);
+  return poop(item);
+};
