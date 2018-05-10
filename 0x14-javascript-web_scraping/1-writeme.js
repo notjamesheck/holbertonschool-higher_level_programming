c5ddcf67e331ad116399a6ce4c29b1c30ab7a15b
notjamesheck <224@holbertonschool.com>
2018-05-09 23:18:38 -0700

    exercise 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,2 +1,12 @@
 #!/usr/bin/node
 // comment
+
+let f = require('fs');
+
+let path = process.argv[2];
+
+let string = process.argv[3];
+
+f.writeFile(path, string, 'utf-8', function (error) {
+  if (error) throw error;
+});
