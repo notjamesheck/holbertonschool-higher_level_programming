f6144c705c16ce49ec8cd3c6d5251466776aebfa
notjamesheck <224@holbertonschool.com>
2018-05-09 20:52:04 -0700

    exercise 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+// returns the reversed version of a list
+// https://alligator.io/js/slice-vs-splice/
+
+exports.esrever = function (list) {
+  for (let i = 0; i < list.length - 1; i++) {
+    // inserts elem. at i, no elem. removed
+    list.splice(i, 0, list.pop());
+  }
+  return list;
+}
+;
