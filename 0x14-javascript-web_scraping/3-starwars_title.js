7a2dd85568866dcee8fa05b5a3e90fef87aba1cb
notjamesheck <224@holbertonschool.com>
2018-05-10 10:08:07 -0700

    exercise 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,2 +1,11 @@
 #!/usr/bin/node
 // comment
+
+const ep = `http://swapi.co/api/films/${process.argv[2]}`;
+
+let req = require('request');
+
+req({ uri: ep, method: 'GET', json: true }, function (error, response, body) {
+  if (error) throw error;
+  console.log(body.title);
+});
