be277d3be2b2c3343b8ccd8bd604ba818dc66c07
notjamesheck <224@holbertonschool.com>
2018-05-09 22:54:23 -0700

    exercise 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,2 +1,11 @@
 #!/usr/bin/node
 // comment
+
+let f = require('fs');
+f.readFile(`${process.argv[2]}`, 'utf-8', function (error, data) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(data);
+  }
+});
