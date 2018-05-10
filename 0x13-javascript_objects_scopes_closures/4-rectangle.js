9e9cedb0686468e9ba5e99f0bee19e20368f887b
notjamesheck <224@holbertonschool.com>
2018-05-09 19:23:13 -0700

    exercise 4

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,1 +1,35 @@
 #!/usr/bin/node
+// create Rectangle class
+// https://stackoverflow.com/questions/42684177/node-js-es6-classes-with-require
+
+class Rectangle {
+  constructor (w, h) {
+    if (!w || !h) {
+      return;
+    }
+    if (w <= 0 || h <= 0) {
+      return;
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log(Array(this.width + 1).join('X'));
+    }
+  }
+
+  rotate () {
+    let temp = this.height;
+    this.height = this.width;
+    this.width = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
