8aeae199894e9c10ebfd79432599fd29da0dd844
notjamesheck <224@holbertonschool.com>
2018-05-10 09:52:02 -0700

    exercise 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 // comment
 
-const url = process.argv[2];
+const path = process.argv[2];
 
 let req = require('request');
 
-req(url, function (err, response, body) {
-  if (err) throw err;
-  console.log('code: ', response.statusCode);
+req({ uri: path, method: 'GET' }, function (error, response, body) {
+  if (error) throw error;
+  console.log(`code: ${response.statusCode}`);
 });
