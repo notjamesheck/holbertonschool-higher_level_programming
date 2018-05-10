64ebb645ddf0d8cf4a996c489213d9dd0a98b288
notjamesheck <224@holbertonschool.com>
2018-05-09 20:16:35 -0700

    exercise 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+// extends method to square object
+
+const S = require('./5-square');
+
+class Square extends S {
+  charPrint (c = 'X') {
+    for (let i = 0; i < this.height; i++) {
+      console.log(Array(this.width + 1).join(c));
+    }
+  }
+}
+
+module.exports = Square;
