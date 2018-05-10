f39d09029afcd2cc97207c3cac029a02b4de40ca
notjamesheck <224@holbertonschool.com>
2018-05-10 10:46:31 -0700

    exercise 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,2 +1,19 @@
 #!/usr/bin/node
 // comment
+
+const ep = process.argv[2];
+
+const wA = 'https://swapi.co/api/people/18/';
+
+let req = require('request');
+
+req({ uri: ep, method: 'GET', json: true }, function (error, response, body) {
+  if (error) throw error;
+  let c = 0;
+  for (let i = 0; i < body.results.length - 1; i++) {
+    c += body.results[i].characters.filter(function (val) {
+      return val === wA;
+    }).length;
+  }
+  console.log(c);
+});
