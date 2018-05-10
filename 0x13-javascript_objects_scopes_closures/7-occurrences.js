3363041a8af708fc1320286f7a595059b223adab
notjamesheck <224@holbertonschool.com>
2018-05-09 20:36:22 -0700

    exercise 7

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+// number of occurrences in a list
+
+exports.nbOccurences = function (list, searchElement) {
+  return list.filter(function (val) {
+    return val === searchElement;
+  }).length;
+};
